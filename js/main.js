7c727b353e2dc72b28bb79935523a197622f706c
tacur <36847088+tacur@users.noreply.github.com>
2019-10-27 22:28:53 +0100

    sfsf

--- js/main.js
+++ js/main.js
@@ -51,30 +51,8 @@
     });
 
 });*/
-// ####### Frontpage mit Menu/Navbar:
   }(jQuery));
-  (function() {
-	var pushingNavTrigger = document.getElementsByClassName('js-cd-nav-trigger');
-	
-	if(pushingNavTrigger.length > 0) {
-		var mainContent = document.getElementsByClassName('cd-main__content')[0],
-			navAnimating = false;
-		
-		pushingNavTrigger[0].addEventListener('click', function(event) {
-			event.preventDefault();
-			if(navAnimating) return; // already animating -> do not toggle
-			navAnimating = true;
-			Util.toggleClass(document.body, 'nav-is-open', !Util.hasClass(document.body, 'nav-is-open'));
-		});
 
-		mainContent.addEventListener('transitionend', function(){
-			navAnimating = false; // wait for the ened of animation to reset the variable
-		});
-	}
-}());
-
-
-// #######
 (function ($) {
 $(document).ready(function(){
 	$(window).scroll(function () {

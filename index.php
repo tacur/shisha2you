cdb7916e2e2e7e7b8352053f83d76edfce1ed3f8
tacur <36847088+tacur@users.noreply.github.com>
2020-04-03 18:19:29 +0200

    sss

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-include("dbconnect.php");
+$db = mysqli_connect("localhost", "salias", "bxPJgr6km4mx", "shisha2you_bestellungen");
+if(!$db)
+{
+  exit("Verbindungsfehler: ".mysqli_connect_error());
+}
 ?>
 <!doctype html>
 <html lang="en" class="no-js">

e002bb5e214c4260d3b9aa50bd29da913eb8f67e
tacur <36847088+tacur@users.noreply.github.com>
2020-04-03 20:13:27 +0200

    sss

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,21 @@
 <?php
-$db = mysqli_connect("localhost", "salias", "bxPJgr6km4mx", "shisha2you_bestellungen");
+$db = mysqli_connect("localhost", "salia", "bxPJgr6km4mx", "shisha2you_bestellungen");
 if(!$db)
 {
   exit("Verbindungsfehler: ".mysqli_connect_error());
 }
+
+$sql = "SELECT Bestellung, Datum FROM Bestellungen";
+ 
+$db_erg = mysqli_query( $db, $sql );
+if ( ! $db_erg )
+{
+  die('Ungültige Abfrage: ' . mysqli_error());
+}
+
 ?>
+
+<?php while($row = $db_erg->fetch_assoc()) {
+	echo "Bestellung" . $row["bestellung"]. "/ Datum: " . $row["Datum"] . "/ Index: "
+	. $row["Index"]. "";
+	}?>
